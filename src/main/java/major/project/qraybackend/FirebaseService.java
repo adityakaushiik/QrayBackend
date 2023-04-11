6edf784b9d909abb948b1ab8deb23c36ee94b800
Aditya Kaushik <addykaushik23@gmail.com>
2023-04-11 13:13:21 +0530

    minor changes and adding json file

--- src/main/java/major/project/qraybackend/FirebaseService.java
+++ src/main/java/major/project/qraybackend/FirebaseService.java
@@ -1,31 +1,19 @@
 package major.project.qraybackend;
 
 import com.google.api.core.ApiFuture;
-import com.google.auth.oauth2.GoogleCredentials;
 import com.google.cloud.firestore.Firestore;
 import com.google.cloud.firestore.WriteResult;
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.FirebaseOptions;
 import com.google.firebase.cloud.FirestoreClient;
 import org.springframework.stereotype.Service;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.util.Map;
+import java.util.concurrent.ExecutionException;
 
 @Service
 public class FirebaseService{
-    private final FileInputStream refreshToken = new FileInputStream("C:\\Users\\User\\Downloads\\q-ray-firebase-adminsdk.json");
 
-    public FirebaseService() throws FileNotFoundException {
-    }
-
-    public String addData(Map<String, Object> docData , String collectionName , String documentId) throws Exception {
-        FirebaseOptions options = new FirebaseOptions.Builder()
-                .setCredentials(GoogleCredentials.fromStream(refreshToken))
-                .build();
-        FirebaseApp.initializeApp(options);
-
+    public String addData(Map<String, Object> docData , String collectionName , String documentId)
+            throws ExecutionException, InterruptedException {
         Firestore db = FirestoreClient.getFirestore();
 
         ApiFuture<WriteResult> future = db.collection(collectionName).document(documentId).set(docData);
--- src/main/java/major/project/qraybackend/QrayBackendApplication.java
+++ src/main/java/major/project/qraybackend/QrayBackendApplication.java
@@ -1,10 +1,16 @@
 package major.project.qraybackend;
 
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import javax.annotation.PostConstruct;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
@@ -14,7 +20,14 @@ public class QrayBackendApplication {
     @Autowired
     FirebaseService firebaseService ;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        FileInputStream refreshToken = new
+                FileInputStream("src/main/resources/q-ray-firebase-adminsdk.json");
+        FirebaseOptions options = new FirebaseOptions.Builder()
+                .setCredentials(GoogleCredentials.fromStream(refreshToken))
+                .build();
+        FirebaseApp.initializeApp(options);
+
         SpringApplication.run(QrayBackendApplication.class, args);
     }
 
@@ -26,6 +39,6 @@ public class QrayBackendApplication {
         docData.put("country", "USA");
         docData.put("regions", Arrays.asList("west_coast", "social"));
 
-        System.out.println(firebaseService.addData(docData, "users", "abc"));
+        System.out.println(firebaseService.addData(docData, "users", "abc1"));
     }
 }
